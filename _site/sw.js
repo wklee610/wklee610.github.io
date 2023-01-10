4f97f120009671c8400529076cdc3588c2119002
wklee610 <wklee610@naver.com>
2023-01-10 21:53:17 +0900

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230110.215110';
+const cacheName = 'chirpy-20230110.215304';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
