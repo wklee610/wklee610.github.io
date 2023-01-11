cd84d382e683238ac8e6a4e1328daa0180caaf66
wklee610 <wklee610@naver.com>
2023-01-11 22:39:14 +0900

    update sidebar & description

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230110.222713';
+const cacheName = 'chirpy-20230111.223655';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
